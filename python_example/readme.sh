3a0703b209eb22f306243ab9d5f25d7515e1b432
jorge <jorgeloviedo@hotmail.com>
2018-05-25 17:38:55 -0300

    update

---
+++ python_example/readme.sh
@@ -0,0 +1,7 @@
+# Python Example.-
+https://github.com/mozilla/geckodriver
+geckodriver &
+python test.py --binary /home/user/Documents/geckodriver --host 127.0.0.1 --port 4444 --log info
+export PATH=$PATH:/home/user/Documents/automation/python_example/
+import time
+time.sleep(12)
