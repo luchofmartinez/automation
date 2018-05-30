dd33d7c9e5f74b240337e0502027f86039812f08
Luciano <luchofmartinez@gmail.com>
2018-05-30 07:02:13 -0300

    Create methods with comment "To define" in CrudPages

--- ecommerce/src/main/java/com/claro/ecommerce/pages/CrudPage.java
+++ ecommerce/src/main/java/com/claro/ecommerce/pages/CrudPage.java
@@ -1,9 +1,85 @@
 package com.claro.ecommerce.pages;
 
+/**
+ * This class
+ */
 public class CrudPage {
 
-    public void abrirPagina(){
+    /**
+     * To define
+     */
+    public void abrirPagina() {
 
     }
 
+    /**
+     * To define
+     */
+    public void clickOnModalButton() {
+
+    }
+
+    /**
+     * To define
+     */
+    public void writeUrl() {
+
+    }
+
+    /**
+     * To define
+     */
+    public void writeAlphanumeric() {
+
+    }
+
+    /**
+     * To define
+     */
+    public void writeNumeric() {
+
+    }
+
+    /**
+     * To define
+     */
+    public void writeEmail() {
+
+    }
+
+    /**
+     * To define
+     */
+    public void writePassword() {
+
+    }
+
+    /**
+     * To define
+     */
+    public void selectOption() {
+
+    }
+
+    /**
+     * To define
+     */
+    public void writeComment() {
+
+    }
+
+    /**
+     * To define
+     * @param option
+     */
+    public void clickOnOptionButton(int option) {
+
+    }
+
+    /**
+     * To define
+     */
+    public void AllIsOk() {
+
+    }
 }
